1c2083ff7df9b20fe242c91960019982b265a8c3
Yutian Yang <70208693+YutianYang709631@users.noreply.github.com>
2023-02-15 13:54:50 -0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/utils.dart';
+import 'package:myapp/page-1/post-assessment-screen.dart';
 
-import './screens/splash_screen.dart';
-import './screens/assessment_screen.dart';
-import './screens/home_screen.dart';
-import './screens/assessment_prompt_screen.dart';
-import './screens/auth_screen.dart';
+void main() => runApp(MyApp());
 
-void main() => runApp(PorterWellness());
-
-class PorterWellness extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _PorterWellnessApp();
-}
-
-class _PorterWellnessApp extends State<PorterWellness> {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initialization,
-      builder: (ctx, appSnapshot) => MaterialApp(
-        home: appSnapshot.connectionState != ConnectionState.done
-            ? SplashScreen()
-            : StreamBuilder(
-                stream: FirebaseAuth.instance.authStateChanges(),
-                builder: (ctx, userSnapshot) {
-                  // checks if we are logged in, then show the Chat Screen
-                  if (userSnapshot.hasData) {
-                    return HomeScreen();
-                  }
-                  return AuthScreen();
-                },
-              ),
-        theme: ThemeData(
-          primaryColor: const Color.fromRGBO(255, 247, 237, 1),
-          cardColor: const Color.fromRGBO(57,114,105,0.83),
-          buttonColor: const Color.fromRGBO(57, 114, 105, 1),
-          buttonTheme: ButtonTheme.of(context).copyWith(
-            buttonColor: const Color.fromRGBO(57, 114, 105, 1),
-            textTheme: ButtonTextTheme.primary,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-          ),
-        ),
-        routes: {
-          AssessmentScreen.routeName: (ctx) => AssessmentScreen(),
-          HomeScreen.routeName: (ctx) => HomeScreen(),
-          AssessmentPrompt.routeName: (ctx) => AssessmentPrompt(),
-        },
-      ),
-    );
-  }
+class MyApp extends StatelessWidget {
+	@override
+	Widget build(BuildContext context) {
+	return MaterialApp(
+		title: 'Flutter',
+		debugShowCheckedModeBanner: false,
+		scrollBehavior: MyCustomScrollBehavior(),
+		theme: ThemeData(
+		primarySwatch: Colors.blue,
+		),
+		home: Scaffold(
+		body: SingleChildScrollView(
+			child: Scene(),
+		),
+		),
+	);
+	}
 }
